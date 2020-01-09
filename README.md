595558148885d05d4d8f1f89e25c5a062b95519e
Martin Aeschlimann <martinae@microsoft.com>
2020-01-09 22:32:18 +0100

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-TypeScript plugin that provides richer semantic highlighing
+A [TypeScript plugin](https://github.com/Microsoft/TypeScript/wiki/Writing-a-Language-Service-Plugin) that replaces `getEncodedSemanticClassifications` and `getEncodedSyntacticClassifications` to provide more classifications.
+
+New clssifications are returned. Each classification is a combination of a TokenType with any number of TokenModifiers.
+
+```ts
+const enum TokenType {
+	'class' = 0x1000,
+	'enum' = 0x2000,
+	'interface' = 0x3000,
+	'namespace' = 0x4000,
+	'typeParameter' = 0x5000,
+	'type' = 0x6000,
+	'parameter' = 0x7000,
+	'variable' = 0x8000,
+	'property' = 0x9000,
+	'constant' = 0xA000,
+	'function' = 0xB000,
+	'member' = 0xC000,
+}
+
+const enum TokenModifier {
+	'declaration' = 0x0001,
+	'static' = 0x0002,
+	'async' = 0x0004
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -6,40 +6,40 @@
 import * as ts from 'typescript/lib/tsserverlibrary';
 
 export = function init(/*{ typescript }: { typescript: typeof ts_module }*/) {
-    console.log('ts-sh activated');
-    return {
-        create(info: ts.server.PluginCreateInfo) {
-            return decorate(info.languageService);
-        },
-        onConfigurationChanged(_config: any) {
-        },
-    };
+	console.log('ts-sh activated');
+	return {
+		create(info: ts.server.PluginCreateInfo) {
+			return decorate(info.languageService);
+		},
+		onConfigurationChanged(_config: any) {
+		},
+	};
 };
 
 function decorate(languageService: ts.LanguageService) {
 
-    const intercept: Partial<ts.LanguageService> = Object.create(null);
+	const intercept: Partial<ts.LanguageService> = Object.create(null);
 
-    intercept.getEncodedSemanticClassifications = (filename: string, span: ts.TextSpan) => {
-        return {
-            spans: getSemanticTokens(languageService, filename, span),
-            endOfLineState: ts.EndOfLineState.None
-        }
-    };
+	intercept.getEncodedSemanticClassifications = (filename: string, span: ts.TextSpan) => {
+		return {
+			spans: getSemanticTokens(languageService, filename, span),
+			endOfLineState: ts.EndOfLineState.None
+		}
+	};
 
-    intercept.getEncodedSyntacticClassifications = (_filename: string, _span: ts.TextSpan) => {
-        return {
-            spans: [],
-            endOfLineState: ts.EndOfLineState.None
-        }
-    };
+	intercept.getEncodedSyntacticClassifications = (_filename: string, _span: ts.TextSpan) => {
+		return {
+			spans: [],
+			endOfLineState: ts.EndOfLineState.None
+		}
+	};
 
 
-    return new Proxy(languageService, {
-        get: (target: any, property: keyof ts.LanguageService) => {
-            return intercept[property] || target[property];
-        },
-    });
+	return new Proxy(languageService, {
+		get: (target: any, property: keyof ts.LanguageService) => {
+			return intercept[property] || target[property];
+		},
+	});
 }
 
 function getSemanticTokens(jsLanguageService: ts.LanguageService, fileName: string, span: ts.TextSpan): number[] {
@@ -52,36 +52,37 @@ function getSemanticTokens(jsLanguageService: ts.LanguageService, fileName: stri
 		const typeChecker = program.getTypeChecker();
 
 		function visit(node: ts.Node) {
-            if (!node || !ts.textSpanIntersectsWith(span, node.pos, node.getFullWidth())) {
-                return;
-            }
+			if (!node || !ts.textSpanIntersectsWith(span, node.pos, node.getFullWidth())) {
+				return;
+			}
 			if (ts.isIdentifier(node)) {
 				const symbol = typeChecker.getSymbolAtLocation(node);
 				if (symbol) {
 					const decl = symbol.valueDeclaration || symbol.declarations && symbol.declarations[0];
 					if (decl) {
 						let typeIdx = tokenFromDeclarationMapping[decl.kind];
-						let modifierSet = 0;
-						if (node.parent) {
-							const parentTypeIdx = tokenFromDeclarationMapping[node.parent.kind];
-							if (parentTypeIdx === typeIdx && (<ts.NamedDeclaration>node.parent).name === node) {
-								modifierSet = (1 << TokenModifier.declaration);
+						if (typeIdx) {
+							let modifierSet = 0;
+							if (node.parent) {
+								const parentTypeIdx = tokenFromDeclarationMapping[node.parent.kind];
+								if (parentTypeIdx === typeIdx && (<ts.NamedDeclaration>node.parent).name === node) {
+									modifierSet = TokenModifier.declaration;
+								}
 							}
-						}
-						const modifiers = ts.getCombinedModifierFlags(decl);
-						if (modifiers & ts.ModifierFlags.Static) {
-							modifierSet |= (1 << TokenModifier.static);
-						}
-						if (modifiers & ts.ModifierFlags.Async) {
-							modifierSet |= (1 << TokenModifier.async);
-						}
-						if (typeIdx !== undefined) {
-							resultTokens.push(node.getStart(), node.getWidth(), ((typeIdx + 1) << 8) + modifierSet);
+							const modifiers = ts.getCombinedModifierFlags(decl);
+							if (modifiers & ts.ModifierFlags.Static) {
+								modifierSet |= TokenModifier.static;
+							}
+							if (modifiers & ts.ModifierFlags.Async) {
+								modifierSet |= TokenModifier.async;
+							}
+							if (typeIdx !== undefined) {
+								resultTokens.push(node.getStart(), node.getWidth(), typeIdx + modifierSet);
+							}
 						}
 					}
 				}
 			}
-
 			ts.forEachChild(node, visit);
 		}
 		const sourceFile = program.getSourceFile(fileName);
@@ -93,27 +94,25 @@ function getSemanticTokens(jsLanguageService: ts.LanguageService, fileName: stri
 	return resultTokens;
 }
 
-enum TokenType {
-	'class',
-	'enum',
-	'interface',
-	'namespace',
-	'typeParameter',
-	'type',
-	'parameter',
-	'variable',
-	'property',
-	'constant',
-	'function',
-	'member',
-	_sentinel
+const enum TokenType {
+	'class' = 0x1000,
+	'enum' = 0x2000,
+	'interface' = 0x3000,
+	'namespace' = 0x4000,
+	'typeParameter' = 0x5000,
+	'type' = 0x6000,
+	'parameter' = 0x7000,
+	'variable' = 0x8000,
+	'property' = 0x9000,
+	'constant' = 0xA000,
+	'function' = 0xB000,
+	'member' = 0xC000,
 }
 
-enum TokenModifier {
-	'declaration',
-	'static',
-	'async',
-	_sentinel
+const enum TokenModifier {
+	'declaration' = 0x0001,
+	'static' = 0x0002,
+	'async' = 0x0004
 }
 
 const tokenFromDeclarationMapping: { [name: string]: TokenType } = {
